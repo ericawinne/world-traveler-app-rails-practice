cac6659d78a14f01202497cb5b4453d68d775cf4
Erica Winne <erica.winne@powerhrg.com>
2021-10-04 12:10:15 -0400

    done! Jon, Drew, Erica pairing

--- app/controllers/travelers_controller.rb
+++ app/controllers/travelers_controller.rb
@@ -2,4 +2,9 @@ class TravelersController < ApplicationController
   def index
     @travelers = Traveler.all
   end
+
+  def show
+    @traveler = Traveler.find(params[:id])
+  end
+  
 end
---
+++ app/models/vacation.rb
@@ -0,0 +1,5 @@
+class Vacation < ApplicationRecord
+  belongs_to :traveler
+  belongs_to :country
+  
+end
